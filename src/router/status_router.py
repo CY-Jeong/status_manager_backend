761cbf8f78d635a339dcc053daccb1be42be1955
CY-Jeong <cksdid4993@gmail.com>
2021-12-03 04:41:40 -0800

    latest

--- src/router/status_router.py
+++ src/router/status_router.py
@@ -6,5 +6,24 @@ router = APIRouter(
 )
 
 @router.get(
-    ""
+    "/me",
+    responses={200: {"description": "ok", "model": HealthStatusOutDto}},
 )
+async def index(
+    status_service: StatusService = Depends(StatusService),
+) -> HealthStatusOutDto:
+    health_status = await status_service.health_check()
+
+    return HealthStatusOutDto(**health_status)
+
+
+@router.get(
+    "/gpu/{gpu_id}",
+)
+async def get_gpu_status(
+    gpu_id: str, http_client: aiohttp.ClientSession = Depends(http_client)
+):
+    res = await http_client.get(
+        os.path.join(os.getenv("GPU_MANAGER_1"), "status", "node"),
+    )
+    return await res.json()
